6efee3defebbcb83e0b7a825df741d3810aa02eb
miguelluna93 <miguelitoo13@hotmail.com>
2013-12-12 19:51:45 -0600

    sesion

---
+++ sesion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body background="fondo1.jpg">
+  <center>
+    <h1 style="color:#33ff33">Programacion IV</h1>
+         
+         <fieldset style="width:550px; border:4px solid #40a800; background:#34526f;">
+      <legend style="color:#33ffcc;"><h2>Iniciar sesion </h2></legend>
+          
+          <span style="color: #ffffff;">
+          <DIV ALIGN=left>
+    <form method="post" action="datos.php">
+      
+          <label for="txtCodigo">Codigo </label><br/>
+          <input type="text" name ="txtCodigo"/><p />
+          
+      
+          <label for="txtCodigo">Password</label><br/>
+          <input type="password" name="txtPass" /><p />
+          </span>
+          <br/>
+      <br/>
+      </DIV>
+          <center>
+          <input type="submit" value="Iniciar sesion" />
+    </form>
+         </center>
+     </fieldset>
+  </body>
+</html>
